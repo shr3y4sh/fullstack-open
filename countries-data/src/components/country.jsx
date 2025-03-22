366bf9292f165bb9719f173a5ad78cab700481f2
shr3y4sh <67783996+shr3y4sh@users.noreply.github.com>
2025-03-22 14:45:41 +0530

    weather api add

--- countries-data/src/components/country.jsx
+++ countries-data/src/components/country.jsx
@@ -1,4 +1,10 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const weatherQuery_uri =
+	'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/';
+
+let weatherData = null;
 
 export default function CountryDescription({ country, isList }) {
 	const [showView, setShowView] = useState(false);
@@ -10,6 +16,25 @@ export default function CountryDescription({ country, isList }) {
 	if (!isList && !showView) {
 		setShowView(true);
 	}
+
+	useEffect(() => {
+		axios
+			.get(
+				`${weatherQuery_uri}/${country.capital[0]}?key=${
+					import.meta.env.VITE_WEATHER_APIKEY
+				}`
+			)
+			.then((response) => {
+				return response.data.currentConditions;
+			})
+			.then((data) => {
+				weatherData = data;
+				console.log(weatherData);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, [country.capital]);
 
 	return !showView ? (
 		<>
@@ -38,6 +63,16 @@ export default function CountryDescription({ country, isList }) {
 					alt={`Flag of ${country.name.common}`}
 				/>
 			</div>
+			{weatherData !== null && (
+				<>
+					<h1>Weather conditions</h1>
+
+					<p>dateTime: {weatherData.datetime}</p>
+					<p>temperature: {weatherData.temp} Celsius</p>
+					<p>conditions: {weatherData.conditions}</p>
+					<p>{weatherData.icon}</p>
+				</>
+			)}
 		</>
 	);
 }
